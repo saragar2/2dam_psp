eac2204f248e9920dbcd0c3a2d402ec0ad0ed703
Sara García Miguel <146083097+saragar2@users.noreply.github.com>
2025-10-14 10:43:11 +0200

    un chikicambio para que se vea más el concepto

--- ex_2_6.py
+++ ex_2_6.py
@@ -1,14 +1,16 @@
 from queue import Queue
 from threading import Thread
-import random
+import random, time
 
 def producir(cantidad, cola):
     for i in range (cantidad):
-        # cola.put(random.randint(1, 10))
+        print(f"Produciendo el número {i}")
+        time.sleep(random.randint(1, 3)) #para añadir realismo, se tarda en producir
         cola.put(i)
 
 def consumir(cantidad, cola):
     for i in range (cantidad):
+        time.sleep(2) #también se tarda en recibir
         print(f"Número recibido: {cola.get()}")
 if __name__ == "__main__":
     cantidad = 10
